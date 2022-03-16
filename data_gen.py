46b1e88e947cfd42a553ca9576d7bebf8dbbf7ac
diphosphane <diphosphane@gmail.com>
2022-03-16 12:10:00 +0800

    add pressure test generator

---
+++ data_gen.py
@@ -0,0 +1,93 @@
+import datetime
+import os
+from itertools import combinations
+from functools import reduce
+from random import shuffle
+import numpy as np
+
+
+record = None
+server_num, client_num, time_len = 0, 0, 0
+pressure = 0.8
+qos_lim = 0
+qos = None
+dist_matrix = None
+
+def ask(msg: str, default: int):
+    inputed = input(msg)
+    if inputed.strip():
+        return int(inputed.strip())
+    return default
+
+def read_input():
+    global server_num, client_num, time_len, pressure
+    server_num = ask('input server number (default 100):', 100)
+    client_num = ask('input client number (default 10):', 10)
+    time_len = ask('input time length (default 100):', 100)
+    inp = input('input pressure 0.01-0.99 (default 0.8)')
+    if inp.strip():
+        pressure = float(inp.strip())
+
+def distribute_server():
+    global record, qos_lim, qos
+    qos_lim = np.random.randint(200, 500)
+    qos = np.ceil(np.random.normal(qos_lim, 50, size=(server_num, client_num))).astype('int32')
+    mask = np.random.randn()
+    for t_idx in range(time_len):
+        dis_bd = np.random.randint(500, 300000, size=(server_num, client_num))
+        mask = np.random.randn(server_num, client_num) > 0.35
+        dis_bd = dis_bd * mask
+        record[t_idx] = dis_bd
+
+def gen_client_name(n: int):
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    out = list(combinations(alphabet, 2))
+    out = [ reduce(lambda x, y: x+y, each) for each in out ]
+    out += [ i for i in alphabet ]
+    shuffle(out)
+    return out[:n]
+
+def gen_server_name(n: int):
+    cand = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789'
+    out = list(combinations(cand, 2))
+    out = [ reduce(lambda x, y: x+y, each) for each in out ]
+    shuffle(out)
+    return out[:n]
+
+def output(path: str):
+    curr_time = datetime.datetime(2021, 11, 1, 0, 0)
+    time_sep = datetime.timedelta(minutes=5)
+    sname = gen_server_name(server_num)
+    cname = gen_client_name(client_num)
+    with open(os.path.join(path, 'demand.csv'), 'w') as f:
+        f.write('mtime,' + ','.join(cname) + '\n')
+        time_str = curr_time.strftime("%Y-%m-%dT%H:%M")
+        for t_idx in range(time_len):
+            c_demand = record[t_idx].sum(axis=0)
+            c_demand = [ str(i) for i in c_demand ]
+            f.write(time_str + ',' + ','.join(c_demand) + '\n')
+            curr_time += time_sep
+    with open(os.path.join(path, 'site_bandwidth.csv'), 'w') as f:
+        f.write('site_name,bandwidth\n')
+        for s_idx in range(server_num):
+            bd_used = record.sum(axis=-1)[:, s_idx].max()
+            bd_upper = np.ceil(bd_used / pressure).astype('int32')
+            f.write(f'{sname[s_idx]},{bd_upper}\n')
+    with open(os.path.join(path, 'qos.csv'), 'w') as f:
+        f.write('site_name,' + ','.join(cname) + '\n')
+        for s_idx in range(server_num):
+            s = sname[s_idx]
+            qos_list = qos[s_idx]
+            qos_list = [ str(i) for i in qos_list ]
+            qos_str = ','.join(qos_list)
+            f.write(f'{s},{qos_str}\n')
+    with open(os.path.join(path, 'config.ini'), 'w') as f:
+        f.write('[config]\n')
+        f.write(f'qos_constraint={qos_lim}\n')
+
+if __name__ == '__main__':
+    read_input()
+    record = np.zeros((time_len, server_num, client_num), dtype=np.int32)
+    distribute_server()
+    output('pressure_data')
+    
