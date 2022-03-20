ed2a3a39e95a355965cb19acbd3216c98672c3bf
diphosphane <diphosphane@gmail.com>
2022-03-20 20:23:39 +0800

    modify the bug with the output path for data_gen_simulated

--- data_gen_simulated.py
+++ data_gen_simulated.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     record = np.zeros((time_len, server_num, client_num), dtype=np.int32)
     distribute_server()
     if len(sys.argv) == 1:
-        output('pressure_data')
+        output('simulated_data')
     else:
         try: os.mkdir(sys.argv[1])
         except: pass
