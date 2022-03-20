34ed25a85c70385faba5450dbb9b0a92a7949119
diphosphane <diphosphane@gmail.com>
2022-03-20 13:19:50 +0800

    add simulated data

--- README.md
+++ README.md
@@ -1,20 +1,37 @@
 # CodeCraft2022-PressureGenerator
-a pressure data generator made for CodeCraft-2022 华为CodeCraft2022数据压测生成器
+a pressure data generator made for CodeCraft-2022 华为CodeCraft2022压测数据及模拟数据生成器
 
-## Usage: 用法:
+## Usage: 用法:  
+for debug usage:  Debug压测用法  
 ```bash
-python3 data_gen.py [data_path]
+python3 data_gen_pressure.py [data_path]
 ```
-All data will generated at path "data_path".  `data_path` is optional, if not specified, **pressure_data** will be used.  
-所有数据会生成在“data_path”中。该参数是可选的，如果没有指定，将使用**pressure_data**.  
+for online data simulation:  在线数据模拟用法  
+```bash
+python3 data_gen_simulated.py [data_path]
+```
+
+All data will generated at path "data_path".  `data_path` is optional, if not specified, **pressure_data** or **simulated_data** will be used.  
+所有数据会生成在“data_path”中。该参数是可选的，如果没有指定，将使用**pressure_data** 或 **simulated_data**。
 
 
-Attached a pressure=0.6 data.
-附赠了个压力0.6的数据。  
+Attached a pressure=0.6 debug pressure test data and pressure=0.3 online data simulated data.  
+附赠了个debug用的压力0.6的数据 以及 线上数据模拟的压力为0.3的数据。    
 
 If you can pass the test, but can not pass the online test, then it would be the problem of your program, you can try to generate pressure test data, then use the data that can not pass to debug.  
 如果你可以通过判题器的测试，但却无法通过线上测试，那么你可以尝试着使用压测程序生成数据，使用无法通过测试的数据进行debug。  
 
+## Attention 注意  
+In some cases, you can pass through the official dataset, but may have unnexpected behavior at other data, so data_gen_pressure.py may help you debug.  
+在某些情况下，你可以通过官方的线下数据，但可能会在其他数据中出现分配错误，在这种情况下，data_gen_pressure.py可以帮助到你。  
+
+But if you want to simulate the official data, you should use data_gen_simulated.py. Because I can not know the data distribution online, so I can not ensure the data generated can accurately judge your program.  
+但如果你想要模拟官方的数据，你应该使用data_gen_simulated.py。但因为我无法得知官方的数据分布，所以我也无法保证生成的数据能准确地评判你的程序。  
 ## benchmark 判题器  
 benchmark is avail at [here](https://github.com/diphosphane/CodeCraft2022-benchmark).  
 判题器可在[这](https://github.com/diphosphane/CodeCraft2022-benchmark)下载到。  
+
+## Wechat Reward 微信打赏  
+If you find this tool helpful, please consider buy me a cup of coffee.  
+如果你觉得该工具有用，可以考虑打赏一杯咖啡。  
+![打赏](img/reward.jpg)
---
+++ data_gen_simulated.py
@@ -0,0 +1,108 @@
+import datetime
+import os
+from itertools import combinations
+from functools import reduce
+import math
+import sys
+from random import shuffle
+import numpy as np
+
+
+record = None
+server_num, client_num, time_len = 0, 0, 0
+pressure = 0.3
+qos_lim = 0
+qos = None
+dist_matrix = None
+
+def ask(msg: str, default: int):
+    inputed = input(msg)
+    if inputed.strip():
+        return int(inputed.strip())
+    return default
+
+def read_input():
+    global server_num, client_num, time_len, pressure
+    server_num = ask('input server number (default 100):', 100)
+    client_num = ask('input client number (default 30):', 30)
+    time_len = ask('input time length (default 1000):', 1000)
+    inp = input('input pressure 0.01-0.99 (default 0.3):')
+    if inp.strip():
+        pressure = float(inp.strip())
+
+def distribute_server():
+    global record, qos_lim, qos
+    qos_lim = np.random.randint(150, 300)
+    offset = np.random.randint(30, 100)
+    qos = np.ceil(np.random.normal(qos_lim+offset, 50, size=(server_num, client_num))).astype('int32')
+    mask = np.random.randn()
+    for t_idx in range(time_len):
+        dis_bd = np.random.randint(0, math.ceil(550000 / client_num / 37), size=(server_num, client_num))
+        mask = np.abs(np.random.randn(server_num, client_num)) > 0.27
+        dis_bd = dis_bd * mask
+        record[t_idx] = dis_bd
+
+def gen_client_name(n: int):
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    out = list(combinations(alphabet, 2))
+    out = [ reduce(lambda x, y: x+y, each) for each in out ]
+    out += [ i for i in alphabet ]
+    shuffle(out)
+    out = out[:n]
+    out.sort()
+    return out
+
+def gen_server_name(n: int):
+    cand = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789'
+    out = list(combinations(cand, 2))
+    out = [ reduce(lambda x, y: x+y, each) for each in out ]
+    shuffle(out)
+    out = out[:n]
+    out.sort()
+    return out
+
+def output(path: str):
+    curr_time = datetime.datetime(2021, 11, 1, 0, 0)
+    time_sep = datetime.timedelta(minutes=5)
+    sname = gen_server_name(server_num)
+    cname = gen_client_name(client_num)
+    with open(os.path.join(path, 'demand.csv'), 'w') as f:
+        f.write('mtime,' + ','.join(cname) + '\r\n')
+        for t_idx in range(time_len):
+            time_str = curr_time.strftime("%Y-%m-%dT%H:%M")
+            c_demand = record[t_idx].sum(axis=0)
+            zero_mask = np.abs(np.random.randn(client_num))
+            zero_mask = zero_mask > 1e-2
+            c_demand *= zero_mask
+            c_demand = [ str(i) for i in c_demand ]
+            f.write(time_str + ',' + ','.join(c_demand) + '\r\n')
+            curr_time += time_sep
+    with open(os.path.join(path, 'site_bandwidth.csv'), 'w') as f:
+        f.write('site_name,bandwidth\r\n')
+        for s_idx in range(server_num):
+            bd_used = record.sum(axis=-1)[:, s_idx].max()
+            bd_upper = np.ceil(bd_used / pressure).astype('int32')
+            f.write(f'{sname[s_idx]},{bd_upper}\r\n')
+    with open(os.path.join(path, 'qos.csv'), 'w') as f:
+        f.write('site_name,' + ','.join(cname) + '\r\n')
+        for s_idx in range(server_num):
+            s = sname[s_idx]
+            qos_list = qos[s_idx]
+            qos_list = [ str(i) for i in qos_list ]
+            qos_str = ','.join(qos_list)
+            f.write(f'{s},{qos_str}\r\n')
+    with open(os.path.join(path, 'config.ini'), 'w') as f:
+        f.write('[config]\r\n')
+        f.write(f'qos_constraint={qos_lim}\r\n')
+
+if __name__ == '__main__':
+    read_input()
+    record = np.zeros((time_len, server_num, client_num), dtype=np.int32)
+    distribute_server()
+    if len(sys.argv) == 1:
+        output('pressure_data')
+    else:
+        try: os.mkdir(sys.argv[1])
+        except: pass
+        output(sys.argv[1])
+    
